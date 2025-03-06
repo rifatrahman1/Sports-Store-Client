5c6720a3431289762833248d3d22eff39b9e956c
Rifat Rahman <ahsan30rifat@gmail.com>
2025-03-06 13:26:54 +0600

    added to sports card item deleted successfull

--- src/Components/Sport.jsx
+++ src/Components/Sport.jsx
@@ -1,23 +1,82 @@
+import { Edit, Trash2 } from 'lucide-react';
 import React from 'react';
-import { IoStar } from 'react-icons/io5';
-import { Link } from 'react-router-dom';
+import { FaRegEye } from 'react-icons/fa';
+import { IoPencil, IoStar } from 'react-icons/io5';
+import { MdDelete } from 'react-icons/md';
+import { data, Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
-const Sport = ({sport}) => {
-      const {_id, item_name, price, rating, image, status} = sport || {};
+const Sport = ({ sport, set_sports }) => {
+      const { _id, item_name, price, rating, image, status } = sport || {};
+
+      const handle_delete = (_id) => {
+            console.log(_id);
+            Swal.fire({
+                  title: "Are you sure?",
+                  text: "You won't be able to revert this!",
+                  icon: "warning",
+                  showCancelButton: true,
+                  confirmButtonColor: "#3085d6",
+                  cancelButtonColor: "#d33",
+                  confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                  if (result.isConfirmed) {
+                        fetch(`http://localhost:5000/sports/${_id}`, {
+                              method: 'DELETE',
+                        })
+                              .then(res => res.json())
+                              .then((data) => {
+                                    console.log(data);
+                                    if (data.deletedCount > 0) {
+                                          Swal.fire({
+                                                title: "Deleted!",
+                                                text: "Your sports item has been deleted.",
+                                                icon: "success"
+                                          });
+                                          const remaining = sport.filter((data) => data._id !== _id);
+                                          set_sports(remaining);
+                                    }
+                              })
+                  }
+            });
+      }
       return (
             <div>
-                  <Link to={`/details/${_id}`} key={sport._id} className='bg-[#f5f5f5]'>
-                                    <img className=' w-[360px] h-[330px] rounded-t-lg' src={image} alt="" />
-                                    <div className='space-y-2.5 mt-4 p-5'>
-                                          <h3 className='text-lg !font-semibold'>{item_name}</h3>
-                                          <p className='font-semibold'>Price : {price} $</p>
-                                          <p className='font-semibold flex items-center'>Rating : <span><IoStar className='text-amber-400 text-lg' /></span> {rating}</p>
-                                          <p className={`inline px-3 py-1 rounded-lg font-semibold ${status === "in-stock" ? "bg-green-100 text-green-600" : "bg-red-100 text-red-600"}`}>
-                                                {sport.status}
-                                          </p>
-                                    </div>
+                  <div key={sport._id} className='bg-[#f5f5f5]'>
+                        <img className=' w-[360px] h-[330px] rounded-t-lg' src={image} alt="" />
+                        <div className=' mt-4 p-5 flex items-center justify-between'>
+                              <div className='space-y-4'>
+                                    <h3 className='text-lg !font-semibold'>{item_name}</h3>
+                                    <p className='font-semibold'>Price : {price} $</p>
+                                    <p className='font-semibold flex items-center'>Rating : <span><IoStar className='text-amber-400 text-lg' /></span> {rating}</p>
+                                    <p className={`inline px-3 py-1 rounded-lg font-semibold ${status === "in-stock" ? "bg-green-100 text-green-600" : "bg-red-100 text-red-600"}`}>
+                                          {sport.status}
+                                    </p>
+                              </div>
 
-                              </Link>
+                              <div className='space-y-3.5'>
+                                    <Link to={`/details/${_id}`} className='flex items-center !gap-3'>
+                                          <div className='bg-[#D2B48C] p-2 rounded-lg'>
+                                                <FaRegEye className='text-white ' />
+                                          </div>
+                                          <p className='font-bold text-[#D2B48C]'>View</p>
+                                    </Link>
+                                    <Link to={`/update_coffee/${_id}`} className='flex items-center !gap-3'>
+                                          <div className='bg-black p-2 rounded-lg'>
+                                                <IoPencil className='text-white ' />
+                                          </div>
+                                          <p className='font-bold'>Edit</p>
+                                    </Link>
+                                    <Link >
+                                          <button onClick={() => handle_delete(_id)} className='flex items-center !gap-3 cursor-pointer'>
+                                                <div className='bg-red-500 cursor-pointer p-2 rounded-lg'><MdDelete className='text-white ' /></div>
+                                                <p className='font-bold text-red-500'>Delete</p>
+                                          </button>
+                                    </Link>
+                              </div>
+                        </div>
+
+                  </div>
             </div>
       );
 };
--- src/Components/Sports.jsx
+++ src/Components/Sports.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoStar } from 'react-icons/io5';
 import { Link, useLoaderData } from 'react-router-dom';
 import Sport from './Sport';
 
 const Sports = () => {
       const loader_data = useLoaderData();
-      console.log('Loader data', loader_data);
+      const [sports, set_sports] = useState(loader_data)
       return (
             <div>
                   <h1 className='uppercase tracking-widest text-center text-2xl py-12'>Our Departments</h1>
                   <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 !gap-12'>
                         {
-                              loader_data.map((sport) => <Sport key={sport._id} sport={sport}></Sport>)
+                              sports.map((sport) => <Sport key={sport._id} sport={sport} set_sports={set_sports}></Sport>)
                         }
                   </div>
             </div>
